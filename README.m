786273e2f884c2df0e5ed4eae24fd4d5365375f4
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-04-25 08:45:25 -0700

    changing due date

--- README.m
+++ README.m
@@ -2,7 +2,7 @@ Setup
 % HW 3: Motion Estimation for Video Compression
 % Joseph Morga & Brandon Fong [Frame %d]', i
 % CompE 565 Homework 3
-% Due Date: Sunday, 04/12/2020 at 11:59 pm
+% Due Date: Sunday, 05/3/2020 at 11:59 pm
 % Name & ID : Joseph Morga (817281186), Brandon Fong (819295224)
 % email: jmorga@sdsu.edu, fong.m.brandon97@gmail.com
 % Link to Report: https://docs.google.com/document/d/1igNoW4hbrZj2D_o_9otIu8Gh2rc7JC9kzf6-8vJd-GQ/edit?usp=sharing
